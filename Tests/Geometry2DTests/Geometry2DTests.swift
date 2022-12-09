50b148dd5102901f87eb1a7ae0477f1353224a78
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2022-12-09 13:03:23 -0500

    Create initial project

---
+++ Tests/Geometry2DTests/Geometry2DTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Geometry2D
+
+final class Geometry2DTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Geometry2D().text, "Hello, World!")
+    }
+}
